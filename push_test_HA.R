f15e123963507bd87603efbd120a729eac3ed5a5
Hasan Abdo <hasanabdo@Hasans-MacBook-Pro.local>
2024-06-07 18:06:40 -0400

    push test v1

---
+++ push_test_HA.R
@@ -0,0 +1,16 @@
+#' This is a test script
+#' I will save it as push_test_HA.R
+
+
+
+#It would be more professional to use parameter names 
+myvector <- seq(100, 1, by= -7)
+
+myavg <- mean(myvector)
+myavg
+
+mysum <- sum(myvector)
+mysum
+
+mysd <- sd(myvector)
+mysd
