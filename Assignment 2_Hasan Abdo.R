bfd10277f06523e536aaf5dd762d4557bbfb60cf
Hasan Abdo <hasanabdo@Hasans-MacBook-Pro.local>
2024-06-14 21:50:58 -0400

    Initial commit with Question 1

---
+++ Assignment 2_Hasan Abdo.R
@@ -0,0 +1,12 @@
+#Coding in R Assignment 2 - Hasan Abdo
+
+#Question 1 
+#Prompt the User to enter a three digit positive number
+
+num <- readline(prompt = "Please enter a three digit positive number: ")
+num <- as.numeric(num)
+#Question 2
+#Want to check if the number entered is numeric
+
+
+
