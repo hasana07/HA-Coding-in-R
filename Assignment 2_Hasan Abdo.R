213536e624381fd33b714b7d8b83ad13685298c5
Hasan Abdo <hasanabdo@Hasans-MacBook-Pro.local>
2024-06-14 22:09:08 -0400

    Added question 2

--- Assignment 2_Hasan Abdo.R
+++ Assignment 2_Hasan Abdo.R
@@ -5,8 +5,15 @@
 
 num <- readline(prompt = "Please enter a three digit positive number: ")
 num <- as.numeric(num)
+
 #Question 2
 #Want to check if the number entered is numeric
 
+if (!is.na(num)) {
+  print(paste("The number you have entered is", num))
+} else {
+  print("The value you have entered is not a numeric value.")
+}
 
-
+#Question 3
+#Check for Armstrong number
