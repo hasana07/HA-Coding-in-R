b4289cf0846047b1f1b1c8937f1c33b5f5ef0b81
Hasan Abdo <hasanabdo@Hasans-MacBook-Pro.local>
2024-07-01 00:55:59 -0400

    modified code, created a function to read the word file and added user input as a function as well using a repeat loop

--- Class Assignments /Assignment 3/Assignment 3_Hasan Abdo.R
+++ Class Assignments /Assignment 3/Assignment 3_Hasan Abdo.R
@@ -5,17 +5,40 @@
 #Note to reviewer, you may want to make sure the file words.txt is within your working directory for this code to run.
 #Words generated using a random word generator found on https://www.randomlists.com/random-words , I did add a couple of my own words as well!
 
-#Reading the word list into the program using read.table, where our words are placed in coloumn called words
-wordfile <- read.table("words.txt", header = FALSE, stringsAsFactors = FALSE, col.names = "words")
 
-#Converting the table into a character vector and placing it within variable word_list
-word_list <- wordfile$words
+#Creating a function which will read the words from .txt file, can call on this function later 
+read_words <- function() {
+  
+  #Reading the word list into the program using read.table, where our words are placed in coloumn called words
+  wordfile <- read.table("words.txt", header = FALSE, stringsAsFactors = FALSE, col.names = "words")
+  
+  #Converting the table into a character vector and placing it within variable word_list
+  word_list <- wordfile$words
+  
+  #Function returns vector of strings from the column words, so should return the word list. 
+  return(wordfile$words)
+  
+} #read_words function ends here
 
-#word which will be used for the game
-random_word <- sample(word_list,1)
+#Taking user guesses/input
+#Creating a function for user guesses, which will check whether the user only inputs a single number, and an alphabetical letter
+user_guess <- function() {
+#Adding a repeat loop so the User is prompted till they enter a valid letter
+   repeat {
+#Adding user input and assigning it to variable "guess", tolower function makes sure the values entered are lowercase
+    guess <- tolower(readline(prompt = "Please enter a single letter: "))
+#If statement to check whether input is 1 character and is a letter from a-z, loop breaks if this condition checks
+    if (nchar(guess) == 1 && grepl("[a-z]", guess)) {
+      break #break the loop
+#If condition is not satisfied, loop continues and asks for valid input.
+    } else {
+      cat("Invalid input. Please enter a single letter: ")
+    }
+   }
+#After breaking loop, the function will return the valid value for "guess" variable.
+  return(guess) 
+}
 
-#getting length of random word in order to let the user know 
-word_length <- nchar(random_word)           
-                                                       
-#telling the User how many letters their word has 
-cat("Welcome to Hangman\nYour word has", word_length, "letters.")
+
+
+      
